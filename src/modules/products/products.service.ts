dfa82a3854f2c05c0c68ea057ff7a6ca43453e34
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-19 20:07:16 -0500

    Add: query runners

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,10 +1,10 @@
+import { Product } from './entities/product.entity';
 import { Injectable } from '@nestjs/common';
+import { ProductDetail } from './entities/product-detail.entity';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
-import { ProductDetail } from './entities/product-detail.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { ProductCharacteristics } from './entities';
 
 @Injectable()
@@ -12,39 +12,51 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
-    @InjectRepository(ProductDetail)
-    private readonly productDetailRepository: Repository<ProductDetail>,
-    @InjectRepository(ProductCharacteristics)
-    private readonly productCharacteristicsRepository: Repository<ProductCharacteristics>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
-    const { characteristics, ...productData } = createProductDto;
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    const product = this.productRepository.create(productData);
-    await this.productRepository.save(product);
+    try {
+      const { characteristics, ...productData } = createProductDto;
 
-    const productDetail = new ProductDetail();
-    productDetail.det_name = product.prod_name;
-    productDetail.det_fullname = `${product.prod_name} ${product.prod_detail}`;
-    productDetail.det_price_sale = product.prod_price;
-    productDetail.product = product;
-    await this.productDetailRepository.save(productDetail);
+      // Crear y guardar el producto
+      const product = this.productRepository.create(productData);
+      await queryRunner.manager.save(product);
 
-    if (characteristics && characteristics.length > 0) {
-      const productCharacteristics = characteristics.map(char => {
-        const productCharacteristic = new ProductCharacteristics();
-        productCharacteristic.char_name = char.char_name;
-        productCharacteristic.char_fullname = `${product.prod_name} ${char.char_name}`;
-        productCharacteristic.char_price_base = char.char_price_base;
-        productCharacteristic.char_availability = char.char_availability;
-        productCharacteristic.product = product;
-        return productCharacteristic;
-      });
-      await this.productCharacteristicsRepository.save(productCharacteristics);
+      // Crear y guardar el detalle del producto
+      const productDetail = new ProductDetail();
+      productDetail.det_name = product.prod_name;
+      productDetail.det_fullname = `${product.prod_name} ${product.prod_detail}`;
+      productDetail.det_price_sale = product.prod_price;
+      productDetail.product = product;
+      await queryRunner.manager.save(productDetail);
+
+      // Crear y guardar las características del producto
+      if (characteristics && characteristics.length > 0) {
+        const productCharacteristics = characteristics.map(char => {
+          const productCharacteristic = new ProductCharacteristics();
+          productCharacteristic.char_name = char.char_name;
+          productCharacteristic.char_fullname = `${product.prod_name} ${char.char_name}`;
+          productCharacteristic.char_price_base = char.char_price_base;
+          productCharacteristic.char_availability = char.char_availability;
+          productCharacteristic.product = product;
+          return productCharacteristic;
+        });
+        await queryRunner.manager.save(productCharacteristics);
+      }
+
+      await queryRunner.commitTransaction();
+      return product;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
-
-    return product;
   }
 
   findAll() {
